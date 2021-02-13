b59ea7e5f4bd01d3b3bd7603843d525a9c179867
Cristina Caballero <cristina.caballero2406@gmail.com>
2021-02-13 22:27:38 +0100

    Recursos
    Recursos para desarrollar

---
+++ src/basementaldrugs/bmd_errorlogging.py
@@ -0,0 +1,6 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.7 (3394)
+# Decompiled from: Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)]
+# Embedded file name: .\BasementalDrugs_7.13.134-PUBLIC\bmd_errorlogging.py\bmd_errorlogging.py
+# Compiled at: 2020-05-16 00:05:10
+# Size of source mod 2**32: 3294 bytes
---
+++ src/basementaldrugs/bmd_version.py
@@ -0,0 +1,6 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 3.7 (3394)
+# Decompiled from: Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)]
+# Embedded file name: .\BasementalDrugs_7.13.134-PUBLIC\bmd_version.py\bmd_version.py
+# Compiled at: 2020-11-24 13:39:04
+# Size of source mod 2**32: 4536 bytes
